f7aba2b69630c2834bcfd39f0c15662f632df821
john-bacic <151886958+john-bacic@users.noreply.github.com>
2024-07-20 18:02:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -30,32 +30,3 @@ function newMessage() {
   document.getElementById('message').value = ''
 }
 
-// function focusTextarea() {
-//   const textarea = document.getElementById('message')
-
-//   // Create a temporary input field
-//   const temp = document.createElement('input')
-//   temp.style.position = 'absolute'
-//   temp.style.top = '-1000px'
-//   temp.style.left = '-1000px'
-//   document.body.appendChild(temp)
-
-//   // Focus the temporary input
-//   temp.focus()
-
-//   // Remove the temporary input and focus the textarea
-//   setTimeout(() => {
-//     document.body.removeChild(temp)
-//     textarea.focus()
-//     // Scroll to the textarea to ensure it's in view
-//     textarea.scrollIntoView()
-//   }, 100)
-
-//   // For iOS devices, we can try setting the cursor position
-//   if (/iPad|iPhone|iPod/.test(navigator.userAgent)) {
-//     textarea.selectionStart = textarea.selectionEnd = textarea.value.length
-//   }
-// }
-
-// // Call the function when the page loads
-// window.onload = focusTextarea
