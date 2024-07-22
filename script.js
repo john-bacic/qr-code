a48db249371356893ebaa14932850c96c3e36277
john-bacic <151886958+john-bacic@users.noreply.github.com>
2024-07-21 21:23:31 -0400

    prevent zooming

--- script.js
+++ script.js
@@ -204,10 +204,29 @@ document.addEventListener('DOMContentLoaded', function () {
   })
 })
 
-// Modify your window.onload function to include setupCameraButton
-// window.onload = function () {
-//   displayMessage()
-//   applyRandomRotationToMessageContainer()
-//   applyRandomRotation()
-//   setupCameraButton()
-// }
+// Prevent double-tap to zoom
+document.addEventListener(
+  'touchstart',
+  function (event) {
+    if (event.touches.length > 1) {
+      event.preventDefault()
+    }
+  },
+  { passive: false }
+)
+
+// Prevent double-tap to zoom by disabling double-click
+let lastTouchEnd = 0
+document.addEventListener(
+  'touchend',
+  function (event) {
+    const now = new Date().getTime()
+    if (now - lastTouchEnd <= 300) {
+      event.preventDefault()
+    }
+    lastTouchEnd = now
+  },
+  false
+)
+
+// prevent zooming
