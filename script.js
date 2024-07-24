8189455f204ce611dc1f2e6d084a6da900e6030b
john-bacic <151886958+john-bacic@users.noreply.github.com>
2024-07-24 15:50:19 -0400

    fix the double fade

--- script.js
+++ script.js
@@ -162,23 +162,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 //////////////////////////////
 
-// Function to add fade-out effect
-function addFadeOutEffect(element, callback) {
-  element.classList.remove('fade-in')
-  element.classList.add('fade-out')
-  element.addEventListener('animationend', function handler() {
-    element.removeEventListener('animationend', handler)
-    if (callback) callback()
-  })
-}
-
-// Function to add fade-in effect
-function addFadeInEffect(element) {
-  element.classList.remove('fade-out')
-  void element.offsetWidth // Trigger reflow to restart animation
-  element.classList.add('fade-in')
-}
-
 // Function to update the message with fade-out and fade-in effects
 function updateMessage(newMessage) {
   const messageElement = document.getElementById('message')
